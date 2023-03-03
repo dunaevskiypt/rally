95a365b569c4181734d11ed393bef82930f70aa8
Peter <dunaevskiyp@gmail.com>
2023-03-03 11:13:10 +0200

    responsive

--- js/main.js
+++ js/main.js
@@ -7,3 +7,138 @@ const swiper = new Swiper(".swiper", {
     prevEl: ".swiper-button-left",
   },
 });
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 37.231380056186595, lng: -8.628354609112586 },
+    zoom: 14,
+    styles: [
+      {
+        featureType: "administrative",
+        elementType: "all",
+        styles: [
+          {
+            saturation: "-100",
+          },
+        ],
+      },
+      {
+        featureType: "administrative.province",
+        elementType: "all",
+        stylers: [
+          {
+            visibility: "off",
+          },
+        ],
+      },
+      {
+        featureType: "landscape",
+        elementType: "all",
+        stylers: [
+          {
+            saturation: -100,
+          },
+          {
+            lightness: 65,
+          },
+          {
+            visibility: "on",
+          },
+        ],
+      },
+      {
+        featureType: "poi",
+        elementType: "all",
+        stylers: [
+          {
+            saturation: -100,
+          },
+          {
+            lightness: "50",
+          },
+          {
+            visibility: "simplified",
+          },
+        ],
+      },
+      {
+        featureType: "road",
+        elementType: "all",
+        stylers: [
+          {
+            saturation: "-100",
+          },
+        ],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "all",
+        stylers: [
+          {
+            visibility: "simplified",
+          },
+        ],
+      },
+      {
+        featureType: "road.arterial",
+        elementType: "all",
+        stylers: [
+          {
+            lightness: "30",
+          },
+        ],
+      },
+      {
+        featureType: "road.local",
+        elementType: "all",
+        stylers: [
+          {
+            lightness: "40",
+          },
+        ],
+      },
+      {
+        featureType: "transit",
+        elementType: "all",
+        stylers: [
+          {
+            saturation: -100,
+          },
+          {
+            visibility: "simplified",
+          },
+        ],
+      },
+      {
+        featureType: "water",
+        elementType: "geometry",
+        stylers: [
+          {
+            hue: "#ffff00",
+          },
+          {
+            lightness: -25,
+          },
+          {
+            saturation: -97,
+          },
+        ],
+      },
+      {
+        featureType: "water",
+        elementType: "labels",
+        stylers: [
+          {
+            lightness: -25,
+          },
+          {
+            saturation: -100,
+          },
+        ],
+      },
+    ],
+  });
+}
+
+window.initMap = initMap;
